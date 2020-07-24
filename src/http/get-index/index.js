9504042772f5d9644abbd715f8b9e28d6ef17058
Paul McKellar <paul.mckellar@gmail.com>
2020-07-24 11:58:16 -0700

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,11 +7,11 @@ exports.handler = async function Index () {
     /**
      * Basic bio
      */
-    fullname: 'Your Name', // ←  Start by adding your name!
-    title: 'My personal site!',
+    fullname: 'Paul McKellar', // ←  Start by adding your name!
+    title: 'a site on begin!',
     occupation: 'Artist & Photographer',
-    location: 'West Glacier, MT',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
+    location: 'SF',
+    bio: 'Mostly not an AI',
 
     /**
      * Contact / social
